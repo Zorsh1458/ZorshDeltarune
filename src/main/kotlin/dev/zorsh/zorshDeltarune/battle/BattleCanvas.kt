fea982f3a6e99bd09bb080cea36f9d0eeb4b1d6c
Zorsh <incognito1458@gmail.com>
2026-08-29 14:02:33 +0300

    changes: Player Options buttons

--- src/main/kotlin/dev/zorsh/zorshDeltarune/battle/BattleCanvas.kt
+++ src/main/kotlin/dev/zorsh/zorshDeltarune/battle/BattleCanvas.kt
@@ -360,7 +360,7 @@ class BattleCanvas(val players: List<Player>, val battle: INeverlandBattle) {
                 section("BUTTONS", true) {
                     myCanvas.drawSprite(
                         0f,
-                        -130f,
+                        -133f,
                         1f,
                         1f,
                         57,
