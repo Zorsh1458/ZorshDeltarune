d5e6ad7467eb426b1567cd0a40e06475dc06cff0
Zorsh <incognito1458@gmail.com>
2026-09-01 23:49:28 +0300

    additions: TP Bar outline sprite refactor: CanvasSprite class refactor

--- src/main/kotlin/dev/zorsh/zorshDeltarune/ui/CanvasSprite.kt
+++ src/main/kotlin/dev/zorsh/zorshDeltarune/ui/CanvasSprite.kt
@@ -4,38 +4,22 @@ import dev.zorsh.zorshDeltarune.utils.font
 import net.kyori.adventure.text.Component
 import kotlin.math.max
 
-enum class CanvasSprite {
-    MOUSE,
-    SQUARE,
-    DBUTTON_FIGHT,
-    DBUTTON_ACT,
-    DBUTTON_ITEM,
-    DBUTTON_MERCY,
-    DBUTTON_DEFEND,
-    SLIME_SPRITE_1,
-    SLIME_SPRITE_2,
-    SOUL,
-    SOUL_OTHER,
-    SOUL_OUTLINE,
-    SOUL_OUTLINE_BIG,
-    UNKNOWN;
-
-    fun getSize() = when (this) {
-        MOUSE -> 9 to 9
-        SQUARE -> 2 to 2
-        DBUTTON_FIGHT -> 31 to 26
-        DBUTTON_ACT -> 31 to 26
-        DBUTTON_ITEM -> 31 to 26
-        DBUTTON_MERCY -> 31 to 26
-        DBUTTON_DEFEND -> 31 to 26
-        SLIME_SPRITE_1 -> 34 to 28
-        SLIME_SPRITE_2 -> 30 to 34
-        SOUL -> 20 to 20
-        SOUL_OTHER -> 20 to 20
-        UNKNOWN -> 64 to 32
-        SOUL_OUTLINE -> 40 to 40
-        SOUL_OUTLINE_BIG -> 48 to 48
-    }
+enum class CanvasSprite(private val sizeX: Int, private val sizeY: Int, private val textValue: Char) {
+    UNKNOWN         (64, 32, '\uF000'),
+    SQUARE          (2, 2, '\uF002'),
+    DBUTTON_FIGHT   (31, 26, '\uF003'),
+    DBUTTON_ACT     (31, 26, '\uF004'),
+    DBUTTON_ITEM    (31, 26, '\uF005'),
+    DBUTTON_MERCY   (31, 26, '\uF006'),
+    DBUTTON_DEFEND  (31, 26, '\uF007'),
+    MOUSE           (9, 9, '\uF009'),
+    SLIME_SPRITE_1  (34, 28, '\uF00A'),
+    SLIME_SPRITE_2  (30, 34, '\uF00B'),
+    SOUL            (20, 20, '\uF00C'),
+    SOUL_OTHER      (20, 20, '\uF00D'),
+    SOUL_OUTLINE    (40, 40, '\uF00E'),
+    SOUL_OUTLINE_BIG(48, 48, '\uF00F'),
+    TP_BAR_OUTLINE  (18, 116, '\uF010');
 
     fun applyScaling() = when (this) {
         SQUARE -> false
@@ -44,31 +28,15 @@ enum class CanvasSprite {
 
     fun getSizeRatios(): Pair<Float, Float> {
         if (applyScaling()) {
-            val size = getSize()
-            return size.first.toFloat() / max(1, size.first - 1) to size.second.toFloat() / max(1, size.second - 1)
+            return sizeX.toFloat() / max(1, sizeX - 1) to sizeY.toFloat() / max(1, sizeY - 1)
         }
         return 1f to 1f
     }
 
-    fun isSpriteWidthEven() = getSize().first % 2 == 0
+    fun isSpriteWidthEven() = sizeX % 2 == 0
 
     fun toTextValue(): Component {
-        var text = when(this) {
-            MOUSE -> "\uF009"
-            SQUARE -> "\uF002"
-            DBUTTON_FIGHT -> "\uF003"
-            DBUTTON_ACT -> "\uF004"
-            DBUTTON_ITEM -> "\uF005"
-            DBUTTON_MERCY -> "\uF006"
-            DBUTTON_DEFEND -> "\uF007"
-            SLIME_SPRITE_1 -> "\uF00A"
-            SLIME_SPRITE_2 -> "\uF00B"
-            SOUL -> "\uF00C"
-            SOUL_OTHER -> "\uF00D"
-            SOUL_OUTLINE -> "\uF00E"
-            SOUL_OUTLINE_BIG -> "\uF00F"
-            UNKNOWN -> "\uF000"
-        }
+        var text = "$textValue"
         if (isSpriteWidthEven()) {
             text += '\uF001'
         }
