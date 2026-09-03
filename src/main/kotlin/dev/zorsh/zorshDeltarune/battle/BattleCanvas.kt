468c0bd6d36850902683c388b60c3c947517bcd5
Zorsh <incognito1458@gmail.com>
2026-09-03 22:32:50 +0300

    changes: Buttons UI

--- src/main/kotlin/dev/zorsh/zorshDeltarune/battle/BattleCanvas.kt
+++ src/main/kotlin/dev/zorsh/zorshDeltarune/battle/BattleCanvas.kt
@@ -432,7 +432,7 @@ class BattleCanvas(val players: List<Player>, val battle: INeverlandBattle) {
                     }
 
                     section("BUTTONS", true) {
-                        val buttonColor = ShaderTextColor.pure("#edc4a4")
+                        val buttonColor = ShaderTextColor.pure("#71d9c9")
                         val buttonColorSelected = ShaderTextColor.pure("#ffdd00")
 
                         myCanvas.drawSprite(
