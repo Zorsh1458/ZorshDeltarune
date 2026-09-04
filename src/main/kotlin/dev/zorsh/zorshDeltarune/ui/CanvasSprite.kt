997ecfa8ac504645e0cbe948ceb2a8a20685e34e
Zorsh <incognito1458@gmail.com>
2026-09-04 10:16:51 +0300

    additions: Double resolution button sprites

--- src/main/kotlin/dev/zorsh/zorshDeltarune/ui/CanvasSprite.kt
+++ src/main/kotlin/dev/zorsh/zorshDeltarune/ui/CanvasSprite.kt
@@ -7,10 +7,10 @@ import kotlin.math.max
 enum class CanvasSprite(private val sizeX: Int, private val sizeY: Int, private val textValue: Char) {
     UNKNOWN                 (64, 32, '\uF000'),
     SQUARE                  (2, 2, '\uF002'),
-    DBUTTON_FIGHT           (31, 26, '\uF003'),
-    DBUTTON_ACT             (31, 26, '\uF004'),
-    DBUTTON_ITEM            (31, 26, '\uF005'),
-    DBUTTON_MERCY           (31, 26, '\uF006'),
+    DBUTTON_FIGHT           (62, 52, '\uF003'),
+    DBUTTON_ACT             (62, 52, '\uF004'),
+    DBUTTON_ITEM            (62, 52, '\uF005'),
+    DBUTTON_MERCY           (62, 52, '\uF006'),
     DBUTTON_DEFEND          (62, 52, '\uF007'),
     MOUSE                   (9, 9, '\uF009'),
     SLIME_SPRITE_1          (34, 28, '\uF00A'),
@@ -20,11 +20,11 @@ enum class CanvasSprite(private val sizeX: Int, private val sizeY: Int, private 
     SOUL_OUTLINE            (40, 40, '\uF00E'),
     SOUL_OUTLINE_BIG        (48, 48, '\uF00F'),
     TP_BAR_OUTLINE          (18, 116, '\uF010'),
-    DBUTTON_FIGHT_SELECTED  (31, 26, '\uF011'),
-    DBUTTON_ACT_SELECTED    (31, 26, '\uF012'),
-    DBUTTON_ITEM_SELECTED   (31, 26, '\uF013'),
-    DBUTTON_MERCY_SELECTED  (31, 26, '\uF014'),
-    DBUTTON_DEFEND_SELECTED (31, 26, '\uF015');
+    DBUTTON_FIGHT_SELECTED  (62, 52, '\uF011'),
+    DBUTTON_ACT_SELECTED    (62, 52, '\uF012'),
+    DBUTTON_ITEM_SELECTED   (62, 52, '\uF013'),
+    DBUTTON_MERCY_SELECTED  (62, 52, '\uF014'),
+    DBUTTON_DEFEND_SELECTED (62, 52, '\uF015');
 
     fun applyScaling() = when (this) {
         SQUARE -> false
