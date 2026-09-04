6c77f560f881a8adc8e729fdca331c563ae9297d
Zorsh <incognito1458@gmail.com>
2026-09-04 15:29:24 +0300

    changes: Player disappearing effect

--- src/main/kotlin/dev/zorsh/zorshDeltarune/battle/player/DeltarunePlayer.kt
+++ src/main/kotlin/dev/zorsh/zorshDeltarune/battle/player/DeltarunePlayer.kt
@@ -143,7 +143,7 @@ class DeltarunePlayer(val uuid: UUID) {
                     val oy = ZorshDeltarune.random.nextFloat() * 2f
                     val oz = ZorshDeltarune.random.nextFloat() - 0.5f
                     val l2 = loc + Vector3f(ox, oy, oz)
-                    l2.world.playEffect(l2 + Vector3f(0f, 0.1f, 0f), Effect.ELECTRIC_SPARK, 0, 0)
+                    l2.world.playEffect(l2 + Vector3f(0f, 0.1f, 0f), Effect.COPPER_WAX_ON, 0, 0)
                 }
             }
             myPlayer.teleport(location)
