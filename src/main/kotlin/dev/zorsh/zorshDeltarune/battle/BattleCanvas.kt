79002e3a7898f83c9c9735645d465a566df58401
Zorsh <incognito1458@gmail.com>
2026-09-04 14:49:25 +0300

    changes: Buttons mask

--- src/main/kotlin/dev/zorsh/zorshDeltarune/battle/BattleCanvas.kt
+++ src/main/kotlin/dev/zorsh/zorshDeltarune/battle/BattleCanvas.kt
@@ -434,7 +434,7 @@ class BattleCanvas(val players: List<Player>, val battle: INeverlandBattle) {
                     section("BUTTONS", true) {
                         myCanvas.drawSprite(
                             0f,
-                            -146.5f,
+                            -147f,
                             82f,
                             1.5f,
                             50,
